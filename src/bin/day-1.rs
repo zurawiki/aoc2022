620f92324531553e12dc7fc20e20bc2a7ec7cff2
Roger Zurawicki <roger@zura.wiki>
2022-12-01 23:32:25 -0500

    Add part 1/2 arg logic

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -2,7 +2,18 @@ use std::cmp::max;
 use std::io;
 use std::io::BufRead;
 
-fn main() {
+use clap::Parser;
+
+/// Argument template for Advent of Code
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Specify part to compute (1 or 2)
+    #[arg(short, long, default_value_t = 1)]
+    part: u8,
+}
+
+fn part1() {
     let mut max_sum: u64 = 0;
     let mut curr_sum: u64 = 0;
 
@@ -18,4 +29,16 @@ fn main() {
 
     max_sum = max(max_sum, curr_sum);
     println!("{}", max_sum);
+}
+fn part2() {}
+
+fn main() {
+    let part = Args::parse().part;
+    println!("Parsed part = {}", part);
+
+    match Args::parse().part {
+        1 => part1(),
+        2 => part2(),
+        _ => panic!("part argument not recognized"),
+    }
 }
