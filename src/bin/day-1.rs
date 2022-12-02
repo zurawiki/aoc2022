e42a94a25d8c873c8633b3bd3fc01f2968ccf3a2
Roger Zurawicki <roger@zura.wiki>
2022-12-02 00:00:30 -0500

    complete day 1 part 2

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -30,7 +30,37 @@ fn part1() {
     max_sum = max(max_sum, curr_sum);
     println!("{}", max_sum);
 }
-fn part2() {}
+
+fn max3<T>(vec: &mut Vec<T>, val: T)
+where
+    T: Ord,
+{
+    vec.push(val);
+    vec.sort_by(|a, b| b.cmp(a));
+    vec.truncate(3);
+}
+
+fn part2() {
+    let mut max3_sum: Vec<u64> = vec![0, 0, 0];
+    let mut curr_sum: u64 = 0;
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        if let Ok(value) = line.unwrap().parse::<u64>() {
+            curr_sum += value;
+        } else {
+            max3(&mut max3_sum, curr_sum);
+            curr_sum = 0;
+        }
+    }
+
+    max3(&mut max3_sum, curr_sum);
+    println!(
+        "Array: {:#?}, Sum = {}",
+        max3_sum,
+        max3_sum.iter().sum::<u64>()
+    );
+}
 
 fn main() {
     let part = Args::parse().part;
