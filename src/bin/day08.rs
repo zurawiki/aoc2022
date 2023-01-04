bee541d795ca8ab3e8a84c1287f3226bbcc2f7a7
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-01-04 15:18:50 -0500

    Complete Day 8 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -78,7 +78,52 @@ fn part1() -> color_eyre::Result<()> {
     Ok(())
 }
 
+fn count_view<'a>(values: impl Iterator<Item = &'a u8> + Clone, height: u8) -> usize {
+    let length = values.clone().count();
+
+    let count = values
+        .clone()
+        .position(|&n| n >= height)
+        .map(|i| i + 1)
+        .unwrap_or(length);
+
+    count
+}
+
+fn score_scenery(lines: &Vec<Vec<u8>>, i: usize, j: usize) -> usize {
+    let z_height = lines[i][j];
+
+    let north = count_view(lines[..i].iter().map(|line| &line[j]).rev(), z_height);
+    let south = count_view(lines[i + 1..].iter().map(|line| &line[j]), z_height);
+    let west = count_view(lines[i][..j].iter().rev(), z_height);
+    let east = count_view(lines[i][j + 1..].iter(), z_height);
+
+    north * south * west * east
+}
+
 fn part2() -> color_eyre::Result<()> {
+    let lines: Vec<Vec<u8>> = get_input()
+        .lines()
+        .map(|line| {
+            line.bytes()
+                .filter(|b| b.is_ascii_digit())
+                .map(|b| b - b'0')
+                .collect()
+        })
+        .collect::<Vec<_>>();
+    dbg!(&lines);
+    let width = lines.get(0).unwrap().len();
+    let height = lines.len();
+    dbg!((width, height));
+
+    let mut max: Option<usize> = None;
+
+    for i in 0..width {
+        for j in 0..height {
+            max = max.max(Some(score_scenery(&lines, i, j)));
+        }
+    }
+    dbg!(max);
     Ok(())
 }
 
